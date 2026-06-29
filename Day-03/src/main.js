f4185a2f2599d143e0f9fab2641202e69e4bc839
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-06-29 21:10:23 +0530

    Masking Animation using Stagger

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -7,18 +7,34 @@ import "./style.css";
 
 // if we want to animate the specific element first, we can use the "from" property in the stagger object. The "from" property can take values like "start", "center", "end", or "random" to determine the starting point of the staggered animations.
 
-gsap.to(".box", {
-  x: "70vw",
-  duration: 1,
-  ease: "elastic.inOut",
-  repeat: -1,
-  yoyo: true,
-  delay: 0.6,
-  // stagger: 0.2,
-//   stagger: -0.2,
-stagger:{
-    each: 0.2,
-    // from: "center"
-    // from: "random"
-}
-});
+// gsap.to(".box", {
+//   x: "70vw",
+//   duration: 1,
+//   ease: "elastic.inOut",
+//   repeat: -1,
+//   yoyo: true,
+//   delay: 0.6,
+//   // stagger: 0.2,
+// //   stagger: -0.2,
+// stagger:{
+//     each: 0.2,
+//     // from: "center"
+//     // from: "random"
+// }
+// });
+
+
+// Revealing animation masking effect
+gsap.from("h1 span",{
+    yPercent: 100,
+    opacity: 0,
+    color: "smokewhite",
+    duration: 1.5,
+    ease:"expo.out",
+    // stagger:-0.1,
+    stagger:{
+        each:0.08,
+        from:"random"
+    }
+})
+
