68858359662537356fea082426ce156fec35e795
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-06-30 11:29:57 +0530

    Learned about timeline and position parameters in gsap

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -1,40 +1,34 @@
 import { gsap } from "gsap";
 import "./style.css";
 
-// Stagger is used to create a delay between the animations of multiple elements. It allows you to animate a group of elements with a specified delay between each animation.
+// timeline is a used to create a sequence of animations that can be controlled as a single unit. It allows you to chain multiple animations together and control their timing, order, and playback.
 
-// if we use negative stagger value, the animations will start in reverse order, creating a "backward" effect.
+// without timeline we have to calculate the duration and delay of each animation manually, which can be time-consuming and error-prone.
 
-// if we want to animate the specific element first, we can use the "from" property in the stagger object. The "from" property can take values like "start", "center", "end", or "random" to determine the starting point of the staggered animations.
+// position parameter is used to specify the position of an animation in a timeline.
 
-// gsap.to(".box", {
-//   x: "70vw",
-//   duration: 1,
-//   ease: "elastic.inOut",
-//   repeat: -1,
-//   yoyo: true,
-//   delay: 0.6,
-//   // stagger: 0.2,
-// //   stagger: -0.2,
-// stagger:{
-//     each: 0.2,
-//     // from: "center"
-//     // from: "random"
-// }
-// });
+// types of position parameter are "<" (start at the same time as the previous animation), ">" (start after the previous animation ends), "+=value" (start after a specified delay), "-=value" (start before a specified delay).
+// labels is a way to give a name to a specific point in the timeline, which can be used to reference that point later in the timeline. Labels can be used to create more complex animations and to synchronize multiple animations together.
 
-
-// Revealing animation masking effect
-gsap.from("h1 span",{
-    yPercent: 100,
-    opacity: 0,
-    color: "smokewhite",
-    duration: 1.5,
-    ease:"expo.out",
-    // stagger:-0.1,
-    stagger:{
-        each:0.08,
-        from:"random"
-    }
-})
-
+// const tl = gsap.timeline();
+// tl.to(".box1", {
+//   x: 800,
+//   duration: 1.3,
+//   ease: "power3.inOut",
+//   delay: 0.5,
+// },"chinu")
+//   .to(".box2", {
+//     x: 800,
+//     duration: 1.5,
+//     ease: "power2.inOut",
+//   },"-=0.5")
+//   .to(".box3", {
+//     x: 800,
+//     duration: 1.8,
+//     ease: "power4.inOut",
+//   },"<1")
+//   .to(".box4", {
+//     x: 800,
+//     duration: 2.3,
+//     ease: "power4.inOut",
+//   },"chinu-=0.5");
