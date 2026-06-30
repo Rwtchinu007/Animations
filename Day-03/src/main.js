acff7f5c5d8b62c317f27068df5795c581e9161b
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-06-30 13:55:02 +0530

    Created LandingPage Animation using GSAP

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -1,34 +1,65 @@
 import { gsap } from "gsap";
 import "./style.css";
 
-// timeline is a used to create a sequence of animations that can be controlled as a single unit. It allows you to chain multiple animations together and control their timing, order, and playback.
+let count = 0;
+const loaderCounter = document.querySelector(".loadercount h1");
+const interval = setInterval(() => {
+  count++;
+  loaderCounter.innerHTML = `${count}%`;
+   gsap.fromTo(
+    loaderCounter,
+    { scale: 1.05 },
+    { scale: 1, duration: 0.15, ease: "power1.out" }
+  );
+  if (count === 100) {
+    clearInterval(interval);
+    landingAnimation();
+  }
+}, 25);
 
-// without timeline we have to calculate the duration and delay of each animation manually, which can be time-consuming and error-prone.
-
-// position parameter is used to specify the position of an animation in a timeline.
-
-// types of position parameter are "<" (start at the same time as the previous animation), ">" (start after the previous animation ends), "+=value" (start after a specified delay), "-=value" (start before a specified delay).
-// labels is a way to give a name to a specific point in the timeline, which can be used to reference that point later in the timeline. Labels can be used to create more complex animations and to synchronize multiple animations together.
-
-// const tl = gsap.timeline();
-// tl.to(".box1", {
-//   x: 800,
-//   duration: 1.3,
-//   ease: "power3.inOut",
-//   delay: 0.5,
-// },"chinu")
-//   .to(".box2", {
-//     x: 800,
-//     duration: 1.5,
-//     ease: "power2.inOut",
-//   },"-=0.5")
-//   .to(".box3", {
-//     x: 800,
-//     duration: 1.8,
-//     ease: "power4.inOut",
-//   },"<1")
-//   .to(".box4", {
-//     x: 800,
-//     duration: 2.3,
-//     ease: "power4.inOut",
-//   },"chinu-=0.5");
+function landingAnimation() {
+  const tl = gsap.timeline();
+  tl.to(".loadercount", {
+    opacity: 0,
+    duration: 1.6,
+    ease: "power4.out",
+  })
+    .to(
+      ".loader",
+      {
+        yPercent: -100,
+        duration: 1.6,
+        ease:"expo.out",
+      },
+      "-=0.9",
+    )
+    .from(
+      ".background img",
+      {
+        scale: 1.4,
+        filter: "blur(10px)",
+        filter: "grayscale(100%)",
+        duration: 1.2,
+        ease: "power4.out",
+      },
+      "-=1.1",
+    )
+    .from(
+      ".heading h1",
+      {
+        yPercent: 100,
+        duration: 1.2,
+        ease: "expo.out", 
+      },
+      "-=0.8",
+    )
+    .from(
+      ".subheading h2",
+      {
+        yPercent: 100,
+        duration: 1.2,
+        ease: "expo.out",
+      },
+      "-=0.77",
+    );
+}
