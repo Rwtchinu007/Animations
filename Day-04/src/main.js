72d67a920f8ade7d3565df386892b89d165d73c2
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-02 12:54:04 +0530

    Learned about

--- Day-04/src/main.js
+++ Day-04/src/main.js
@@ -1,16 +1,29 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./style.css";
+gsap.registerPlugin(ScrollTrigger);
+// for scroll trigger we need three things
+// 1. trigger - the element that will trigger the animation
+// 2. start - when the animation should start
+// 3. end - when the animation should end
 
-// Nested timelines are a great way to organize your animations. You can create multiple timelines and then combine them into a master timeline. This allows for better control and sequencing of animations.
+gsap.to(".box", {
+  x: 1250, 
+  ease: "power4.inOut",
+  scrollTrigger: {
+    trigger: ".page2",
+    start: "top top",
+    end: "top -40%",
+    scrub: true, // scrub true means the animation will be linked to the scroll position
+    // scrub value means the animation will be linked to the scroll position but with a delay of that value in seconds
+    pin: true, // pin true means the element will be pinned in place while the animation is running
 
-// // Loading Timeline
-// const loadingTimeline = () =>{
-//     return gsap.timeline().to("Element",{})
-// }
-// // Navbar Timeline
-// const navbarTimeline = () =>{
-//     return gsap.timeline().to("Element",{})
-// }
+    onEnter: ()=>{
+    },
+    onLeave: ()=>{},
+    onEnterBack: ()=>{},
+    onLeaveBack: ()=>{},
+    onUpdate: ()=>{},
 
-// const master = gsap.timeline();
-// master.add(loadingTimeline()).add(navbarTimeline());
+}
+});
