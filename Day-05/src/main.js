c069d1d2b67e35752546523ce7ca1ee7b1b628db
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-07 19:11:43 +0530

    Flip plugin

--- Day-05/src/main.js
+++ Day-05/src/main.js
@@ -1,33 +1,60 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/SplitText";
-import { Draggable } from "gsap/Draggable";
-import { InertiaPlugin } from "gsap/all";
+import { ScrollTrigger, SplitText, Draggable, InertiaPlugin, Flip } from "gsap/all";
 import "./style.css";
 
-gsap.registerPlugin(ScrollTrigger, SplitText, Draggable, InertiaPlugin);
+gsap.registerPlugin(ScrollTrigger, SplitText, Draggable, InertiaPlugin, Flip);
 
-// const split = new SplitText(".title h1", {
-//   type: "chars,words,lines",
-//   wordsClass: "title-word",
-//   charsClass: "title-char",
-// });
-// gsap.from(split.chars, {
-//   yPercent: 100,
-//   opacity: 0,
-//   duration: 1.2,
-//   ease: "bounce.Out",
-//   stagger: {
-//     each: 0.04,
-//     from: "random",
-//   },
-// });
+const imageGallery = document.querySelector(".imageGallery");
+const imageShow = document.querySelector(".imageShow");
 
-Draggable.create(".box", {
-  bounds: "#root",
-  type: "x,y",
-  edgeResistance: 0.9,
-  inertia: true,
-  dragResistance: 0.1  ,
-  // bounds: { minX: 0, maxX: 0, minY: 0, maxY: 0 },
+// Listen for clicks on the image gallery container
+imageGallery.addEventListener("click", (e) => {
+  // Make sure the user actually clicked on an image
+  if (e.target.tagName === "IMG") {
+    const clickedImg = e.target;
+    const currentMainImg = imageShow.querySelector("img");
+
+    // 1. Capture the current layout state of both images
+    const state = Flip.getState([clickedImg, currentMainImg]);
+
+    // 2. Swap them in the DOM
+    // Put the current main image in the gallery where the clicked image used to be
+    imageGallery.insertBefore(currentMainImg, clickedImg);
+    // Put the clicked image into the main show area
+    imageShow.appendChild(clickedImg);
+
+    // 3. Animate the transition between the two states
+    Flip.from(state, {
+      duration: 0.8,
+      ease: "power3.inOut",
+      absolute: true, // Prevents layout jumps during animation
+      scale: true,    // Animates the size smoothly
+      nested: true,   // Important for flexbox containers
+      zIndex: 10,     // Ensures the swapping images appear on top
+    });
+  }
+});
+
+// Optional: Also allow clicking the main image to swap it back with the first image in the gallery
+imageShow.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    const mainImg = e.target;
+    const firstGalleryImg = imageGallery.querySelector("img");
+    
+    if (firstGalleryImg) {
+      const state = Flip.getState([mainImg, firstGalleryImg]);
+      
+      imageGallery.insertBefore(mainImg, firstGalleryImg);
+      imageShow.appendChild(firstGalleryImg);
+      
+      Flip.from(state, {
+        duration: 0.8,
+        ease: "power3.inOut",
+        absolute: true,
+        scale: true,
+        nested: true,
+        zIndex: 10,
+      });
+    }
+  }
 });
