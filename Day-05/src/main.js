3d6b1b78eb5574ec38b8d3f4e4b3ef65f228c678
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-07 18:06:49 +0530

    Draggable and InertiaPlugin

--- Day-05/src/main.js
+++ Day-05/src/main.js
@@ -1,22 +1,33 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
+import { Draggable } from "gsap/Draggable";
+import { InertiaPlugin } from "gsap/all";
 import "./style.css";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, Draggable, InertiaPlugin);
 
-const split = new SplitText(".title h1", {
-  type: "chars,words,lines",
-  wordsClass: "title-word",
-  charsClass: "title-char",
-});
-gsap.from(split.chars, {
-  yPercent: 100,
-  opacity: 0,
-  duration: 1.2,
-  ease: "bounce.Out",
-  stagger: {
-    each: 0.04,
-    from: "random",
-  },
+// const split = new SplitText(".title h1", {
+//   type: "chars,words,lines",
+//   wordsClass: "title-word",
+//   charsClass: "title-char",
+// });
+// gsap.from(split.chars, {
+//   yPercent: 100,
+//   opacity: 0,
+//   duration: 1.2,
+//   ease: "bounce.Out",
+//   stagger: {
+//     each: 0.04,
+//     from: "random",
+//   },
+// });
+
+Draggable.create(".box", {
+  bounds: "#root",
+  type: "x,y",
+  edgeResistance: 0.9,
+  inertia: true,
+  dragResistance: 0.1  ,
+  // bounds: { minX: 0, maxX: 0, minY: 0, maxY: 0 },
 });
